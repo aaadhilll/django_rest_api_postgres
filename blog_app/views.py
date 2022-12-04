ff671d7fe93f9002575ae62f393f8a212a7d8b15
aaadhilll <aratthennala@gmail.com>
2022-12-04 14:41:21 +0530

    JsonResponse Completed

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def blog_list(request):
+    data = {
+        "Message": "Helloworld"
+    }
+    return JsonResponse(data)
