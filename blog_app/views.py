4ba55f0a30b40733d8dee730e3806df0ad302830
ananddgit <anandd.24135@gmail.com>
2022-12-07 22:22:45 +0530

    Serializer

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,21 +1,17 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from .models import Blog
+from .serializer import BlogSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(["GET"])
 def blog_list(request):
-    blogs = Blog.objects.all()
-    
-    data = {
-        "Blogs": list(blogs.values())
-    }
-    return JsonResponse(data)
+    all_blogs = Blog.objects.filter(is_public=True)
+    serializer = BlogSerializer(all_blogs, many=True)
+    return Response(serializer.data)
 
+@api_view(["GET"])
 def blog_detail(request, pk):
-    blogs = Blog.objects.get(pk=pk)
-    
-    data = {
-        "name": blogs.name,
-        "description": blogs.description,
-        "slug": blogs.slug,
-    }
-    return JsonResponse(data)
+    all_blogs = Blog.objects.get(is_public=True, pk=pk)
+    serializer = BlogSerializer(all_blogs)
+    return Response(serializer.data)
